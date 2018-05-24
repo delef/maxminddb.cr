871bed35473ea2df1e7bf162dc040cc376a866e2
delef <delef@ya.ru>
2018-05-25 01:40:56 +0300

    fixes

--- src/maxminddb/format/geoip2/entity/traits.cr
+++ src/maxminddb/format/geoip2/entity/traits.cr
@@ -1,6 +1,11 @@
 module MaxMindDB::Format::GeoIP2::Entity
   class Traits < Base
-    def iso_code
+    def is_anonymous_proxy
+      return unless data = @data
+      data["is_anonymous_proxy"].as_bool
+    end
+
+    def is_satellite_provider
       return unless data = @data
       data["is_satellite_provider"].as_bool
     end
