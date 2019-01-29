9ec190768dbfc5b5e39799fad513ef941314fc15
delef <delef@ya.ru>
2019-01-29 22:43:39 +0300

    simplify spec_helper and drop /spec/databases and /spec/sources

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,34 +1,10 @@
 require "spec"
 require "../src/maxminddb"
 
-Dir.glob(["spec/databases/*.mmdb.gz", "spec/sources/*.json.gz"]) do |file_path|
-  next if File.exists?(file_path.gsub(".gz", ""))
-
-  dir = file_path.split('/').tap(&.pop).join('/')
-
-  unless File.writable?(dir)
-    raise "Invalid `#{dir}` directory permissions"
-  end
-
-  Process.run("sh", {"-c", "gunzip -k #{file_path}"})
-end
-
 def db_path(name : String)
-  path = "spec/databases/#{name}.mmdb"
-
-  unless File.exists?(path)
-    raise "Invalid `#{path}`. No such file or directory."
-  end
-
-  path
+  "spec/data/test-data/#{name}.mmdb"
 end
 
 def source_path(name : String)
-  path = "spec/sources/#{name}.json"
-
-  unless File.exists?(path)
-    raise "Invalid `#{path}`. No such file or directory."
-  end
-
-  path
+  "spec/data/source-data/#{name}.json"
 end
