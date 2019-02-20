3938f21e385b0ddd93c1d8f4f4c7cba13d307fd8
delef <delef@ya.ru>
2019-02-20 14:52:38 +0300

    replace metadata#find_start to buffer.rindex

--- src/maxminddb/metadata.cr
+++ src/maxminddb/metadata.cr
@@ -1,25 +1,26 @@
 require "./any"
 require "./decoder"
-require "benchmark"
 
 module MaxMindDB
   struct Metadata
     private METADATA_START_MARKER = "\xAB\xCD\xEFMaxMind.com".to_slice
 
     getter ip_version : Int32, node_count : Int32, record_size : Int32,
-           build_time : Time, database_type : String, languages : Array(String),
-           description : Hash(String, String), node_byte_size : Int32,
-           search_tree_size : Int32, record_byte_size : Int32,
-           tree_depth : Int32, version : String
+      build_epoch : Time, database_type : String, languages : Array(String),
+      description : Hash(String, String), node_byte_size : Int32,
+      search_tree_size : Int32, record_byte_size : Int32, version : String
 
-    def initialize(bytes : Bytes)
-      start_offset = find_start(bytes)
+    def initialize(buffer : Buffer)
+      metadata_index = buffer.rindex(METADATA_START_MARKER)
 
-      if start_offset.zero?
-        raise InvalidDatabaseException.new("Can't parse binary database")
+      if metadata_index.nil?
+        raise InvalidDatabaseException.new(
+          "Metadata section not found. Is this a valid MaxMind DB file?"
+        )
       end
 
-      metadata = Decoder.new(bytes, start_offset).decode(start_offset).to_any
+      start_offset = metadata_index + METADATA_START_MARKER.size
+      metadata = Decoder.new(buffer, start_offset).decode(start_offset).to_any
 
       if metadata.empty?
         raise InvalidDatabaseException.new("Metadata is empty")
@@ -33,7 +34,7 @@ module MaxMindDB
         raise InvalidDatabaseException.new("Unsupported record size")
       end
 
-      @build_time = Time.unix(metadata["build_epoch"].as_i)
+      @build_epoch = Time.unix(metadata["build_epoch"].as_i)
       @database_type = metadata["database_type"].as_s
       @description = metadata["description"].as_h.transform_values &.as_s
       @languages = metadata["languages"].as_a.map &.as_s
@@ -41,25 +42,11 @@ module MaxMindDB
       @node_byte_size = @record_size / 4
       @search_tree_size = @node_count * @node_byte_size
       @record_byte_size = @node_byte_size / 2
-      @tree_depth = 2 ** (@ip_version + 1) # 32 for IPv4 and 128 for IPv6
 
       @version = [
         metadata["binary_format_major_version"].as_i,
         metadata["binary_format_minor_version"].as_i,
       ].join('.')
-    end
-
-    private def find_start(bytes : Bytes) : Int32
-      marker_size = METADATA_START_MARKER.size
-      start_offset = 0
-
-      (marker_size...bytes.size).reverse_each do |i|
-        if bytes[i - marker_size, marker_size] == METADATA_START_MARKER
-          start_offset = i
-        end
-      end
-
-      start_offset
     end
   end
 end
