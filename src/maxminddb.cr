c90c7177aad5eb10f290eda1c4e24dc201046e69
delef <delef@ya.ru>
2019-02-20 14:48:47 +0300

    add cache_max_size and change input types

--- src/maxminddb.cr
+++ src/maxminddb.cr
@@ -5,7 +5,7 @@ module MaxMindDB
   class InvalidDatabaseException < Exception
   end
 
-  def self.open(db_path : String)
-    Reader.new(db_path)
+  def self.open(input : String | Bytes | IO::Memory, cache_max_size : Int32? = nil)
+    Reader.new(input, cache_max_size)
   end
 end
